1c46210967cfbedff36e7deff84b8c45a90b0d4c
timothybroome <timothy@broome.me>
2022-01-19 13:03:15 +0000

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,166 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-import 'ag-grid-enterprise';
-import { CheckboxRenderer } from './checkbox-renderer.component';
-import { ColDef } from 'ag-grid-community/dist/lib/main';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "ag-grid-enterprise";
+import { CheckboxRenderer } from "./checkbox-renderer.component";
+import { ColDef } from "ag-grid-community/dist/lib/main";
+import { MatCheckboxModule } from "@angular/material/checkbox";
 
 @Component({
-  selector: 'my-app',
+  selector: "my-app",
   template: `
+    <h2 class="title">VOD</h2>
 
+    <div class="page-grid-container xg-frame-2 button-space">
+      <h3 class="title">Ad Server</h3>
 
-<h2 class="title">VOD</h2>
+      <ag-grid-angular
+        #agGrid
+        style="width: 100vw; height: 500px;"
+        id="myGrid"
+        [rowData]="lmkData"
+        class="ag-theme-alpine"
+        [columnDefs]="colDefs"
+        [frameworkComponents]="frameworkComponents"
+      ></ag-grid-angular>
 
-<div class="page-grid-container xg-frame-2 button-space">
-<h3 class="title">Ad Server</h3>
+      <div class="child-panel">
+        <div class="xg-frame-2 half-width-frame">
+          <h4 class="title">General</h4>
 
-  <ag-grid-angular
-    #agGrid
-    style="width: 100vw; height: 500px;"
-    id="myGrid"
-    class="ag-theme-alpine"
-    [defaultColDef]="defaultColDef"
-    [columnDefs]="colDefs"
-    [rowData]="monitorData"
-    [frameworkComponents]="frameworkComponents"
-    [gridOptions]="gridOptions"
-    (gridReady)="onGridReady($event)"
-    ></ag-grid-angular>
+          <div class="xg-text-group" [ngClass]="{ 'xg-required': required }">
+            <label class="xg-text-label" for="inputControl">Name </label>
+            <input
+              type="text"
+              class="xg-text-box"
+              name="textInput"
+              [(ngModel)]="inputModelValue"
+              [placeholder]="placeholder"
+            />
+          </div>
 
-    <div class="child-panel">
-    
-      <div class="xg-frame-2 half-width-frame">
-        <h4 class="title">General</h4>
-      
+          <div class="xg-text-group" [ngClass]="{ 'xg-required': required }">
+            <label class="xg-text-label" for="inputControl">Instruction </label>
+            <textarea
+              type="text"
+              class="xg-text-box"
+              name="textInput"
+              [(ngModel)]="inputModelValue"
+              [placeholder]="placeholder"
+            ></textarea>
+          </div>
+
+          <div class="form-fields-grid">
+            <div class="xg-text-group" [ngClass]="{ 'xg-required': required }">
+              <label class="xg-text-label" for="inputControl"
+                >Advertising Package
+              </label>
+              <input
+                type="text"
+                class="xg-text-box"
+                name="textInput"
+                [(ngModel)]="inputModelValue"
+                [placeholder]="placeholder"
+              />
+            </div>
+
+            <div class="xg-text-group" [ngClass]="{ 'xg-required': required }">
+              <label class="xg-text-label" for="inputControl"
+                >Ad Link Style
+              </label>
+              <input
+                type="text"
+                class="xg-text-box"
+                name="textInput"
+                [(ngModel)]="inputModelValue"
+                [placeholder]="placeholder"
+              />
+            </div>
+          </div>
+
+          <div class="xg-text-group" [ngClass]="{ 'xg-required': required }">
+            <label class="xg-text-label" for="inputControl"
+              >Advertising Space
+            </label>
+            <input
+              type="text"
+              class="xg-text-box"
+              name="textInput"
+              [(ngModel)]="inputModelValue"
+              [placeholder]="placeholder"
+            />
+          </div>
+        </div>
+
+        <div class="xg-frame-2 half-width-frame">
+          <h4 class="title">Advertising Package</h4>
+        </div>
+
+        <div class="xg-frame-2 half-width-frame">
+          <h4 class="title">Content Restriction</h4>
+
+          <div class="checkbox-row">
+
+          <div class="xg-mat-checkbox" [ngClass]="{ 'xg-required': required }">
+            <mat-checkbox
+              [(ngModel)]="checkboxModelValue"
+              [(indeterminate)]="indeterminate"
+              [labelPosition]="labelLeft ? 'before' : 'after'"
+              [required]="required"
+              [disabled]="disabled || readOnly"
+              #xgCheckboxModel="ngModel"
+            >
+              <span
+                [title]="labelName"
+              >
+                Targeted Content
+              </span>
+            </mat-checkbox>
+          </div>
+
+          <div class="xg-mat-checkbox" [ngClass]="{ 'xg-required': required }">
+          <mat-checkbox
+            [(ngModel)]="checkboxModelValue"
+            [(indeterminate)]="indeterminate"
+            [labelPosition]="labelLeft ? 'before' : 'after'"
+            [required]="required"
+            [disabled]="disabled || readOnly"
+            #xgCheckboxModel="ngModel"
+          >
+            <span
+
+              [title]="labelName"
+            >
+              All Content
+            </span>
+          </mat-checkbox>
+        </div>
+
+  
+
+        </div>
+        <ag-grid-angular
+        #agGrid1
+        style="width: 100%; height: 300px;"
+        id="myGrid2"
+        class="ag-theme-alpine"
+        [defaultColDef]="defaultColDef"
+        [columnDefs]="colDefsRestriction"
+        [rowData]="restriction"
+        [frameworkComponents]="frameworkComponents"
+        [gridOptions]="gridOptions"
+        (gridReady)="onGridReady($event)"
+      ></ag-grid-angular>
+  
+        </div>
+        <div class="xg-frame-2 half-width-frame">
+          <h4 class="title">Content Targeting</h4>
+        </div>
       </div>
-
-      <div class="xg-frame-2 half-width-frame">
-        <h4 class="title">Advertising Package</h4>
-      </div>
-
-      <div class="xg-frame-2 half-width-frame">
-        <h4 class="title">Content Restriction</h4>
-      </div>
-      <div class="xg-frame-2 half-width-frame">
-        <h4 class="title">Content Targeting</h4>
-      </div>
-    
     </div>
-
-</div>
   `,
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
   private gridApi;
@@ -66,285 +175,288 @@ export class AppComponent implements OnInit {
   private lmkData: any;
   private colDefs: ColDef[];
 
+  private restriction: any[] = [{"Restriction":"123", "Exclude":"true"}];
+  private colDefsRestriction = [{field: "Restriction"}, {field: "Exclude"}];
+
   private colDefsHC = [
     {
-      field: 'campNo',
+      field: "campNo",
       enableRowGroup: true,
     },
     {
-      field: 'cmodNo',
+      field: "cmodNo",
       enableRowGroup: true,
     },
     {
-      field: 'odlbNo',
+      field: "odlbNo",
       enableRowGroup: true,
     },
     {
-      field: 'progNo',
+      field: "progNo",
       enableRowGroup: true,
     },
     {
-      field: 'progName',
+      field: "progName",
       enableRowGroup: true,
     },
     {
-      field: 'episNo',
+      field: "episNo",
       enableRowGroup: true,
     },
     {
-      field: 'episName',
+      field: "episName",
       enableRowGroup: true,
     },
     {
-      field: 'platNo',
+      field: "platNo",
       enableRowGroup: true,
     },
     {
-      field: 'platName',
+      field: "platName",
       enableRowGroup: true,
     },
     {
-      field: 'priorityInd',
+      field: "priorityInd",
       enableRowGroup: true,
     },
     {
-      field: 'priorityIndText',
+      field: "priorityIndText",
       enableRowGroup: true,
     },
     {
-      field: 'geogNo',
+      field: "geogNo",
       enableRowGroup: true,
     },
     {
-      field: 'geogName',
+      field: "geogName",
       enableRowGroup: true,
     },
     {
-      field: 'exclusiveYn',
+      field: "exclusiveYn",
       enableRowGroup: true,
     },
     {
-      field: 'adspNo',
+      field: "adspNo",
       enableRowGroup: true,
     },
     {
-      field: 'adspName',
+      field: "adspName",
       enableRowGroup: true,
     },
     {
-      field: 'sttDate',
+      field: "sttDate",
       enableRowGroup: true,
     },
     {
-      field: 'endDate',
+      field: "endDate",
       enableRowGroup: true,
     },
     {
-      field: 'duration',
+      field: "duration",
       enableRowGroup: true,
     },
     {
-      field: 'durationAsString',
+      field: "durationAsString",
       enableRowGroup: true,
     },
     {
-      field: 'demoNo',
+      field: "demoNo",
       enableRowGroup: true,
     },
     {
-      field: 'demoShortName',
+      field: "demoShortName",
       enableRowGroup: true,
     },
     {
-      field: 'seqNo',
+      field: "seqNo",
       enableRowGroup: true,
     },
     {
-      field: 'bstpNo',
+      field: "bstpNo",
       enableRowGroup: true,
     },
     {
-      field: 'bstpCode',
+      field: "bstpCode",
       enableRowGroup: true,
     },
     {
-      field: 'budget',
+      field: "budget",
       enableRowGroup: true,
     },
     {
-      field: 'prevBudget',
+      field: "prevBudget",
       enableRowGroup: true,
     },
     {
-      field: 'adlinkStyleNo',
+      field: "adlinkStyleNo",
       enableRowGroup: true,
     },
     {
-      field: 'instructionText',
+      field: "instructionText",
       enableRowGroup: true,
     },
     {
-      field: 'cpt',
+      field: "cpt",
       enableRowGroup: true,
     },
     {
-      field: 'prevCpt',
+      field: "prevCpt",
       enableRowGroup: true,
     },
     {
-      field: 'imprTotTgt',
+      field: "imprTotTgt",
       enableRowGroup: true,
     },
     {
-      field: 'prevImprTotTgt',
+      field: "prevImprTotTgt",
       enableRowGroup: true,
     },
     {
-      field: 'deliveryType',
+      field: "deliveryType",
       enableRowGroup: true,
     },
     {
-      field: 'actualsBillingYn',
+      field: "actualsBillingYn",
       enableRowGroup: true,
     },
     {
-      field: 'prevActualsBillingYn',
+      field: "prevActualsBillingYn",
       enableRowGroup: true,
     },
     {
-      field: 'invoicedYn',
+      field: "invoicedYn",
       enableRowGroup: true,
     },
     {
-      field: 'lineExceptionYn',
+      field: "lineExceptionYn",
       enableRowGroup: true,
     },
     {
-      field: 'externalCampId',
+      field: "externalCampId",
       enableRowGroup: true,
     },
     {
-      field: 'cmodExternalId',
+      field: "cmodExternalId",
       enableRowGroup: true,
     },
     {
-      field: 'cmasExternalId',
+      field: "cmasExternalId",
       enableRowGroup: true,
     },
     {
-      field: 'origMaxImpressions',
+      field: "origMaxImpressions",
       enableRowGroup: true,
     },
     {
-      field: 'maxImpressions',
+      field: "maxImpressions",
       enableRowGroup: true,
     },
     {
-      field: 'imprTotAct',
+      field: "imprTotAct",
       enableRowGroup: true,
     },
     {
-      field: 'nwmdWorkflowstatusNo',
+      field: "nwmdWorkflowstatusNo",
       enableRowGroup: true,
     },
     {
-      field: 'applyAdrestYn',
+      field: "applyAdrestYn",
       enableRowGroup: true,
     },
     {
-      field: 'allContentYn',
+      field: "allContentYn",
       enableRowGroup: true,
     },
     {
-      field: 'name',
+      field: "name",
       enableRowGroup: true,
     },
     {
-      field: 'cmasNameLastSaved',
+      field: "cmasNameLastSaved",
       enableRowGroup: true,
     },
     {
-      field: 'cmasNameLastCalc',
+      field: "cmasNameLastCalc",
       enableRowGroup: true,
     },
     {
-      field: 'adspPackNo',
+      field: "adspPackNo",
       enableRowGroup: true,
     },
     {
-      field: 'adspPackName',
+      field: "adspPackName",
       enableRowGroup: true,
     },
     {
-      field: 'contentGroupNo',
+      field: "contentGroupNo",
       enableRowGroup: true,
     },
     {
-      field: 'contentGroupDescription',
+      field: "contentGroupDescription",
       enableRowGroup: true,
     },
     {
-      field: 'prevSttDate',
+      field: "prevSttDate",
       enableRowGroup: true,
     },
     {
-      field: 'prevEndDate',
+      field: "prevEndDate",
       enableRowGroup: true,
     },
     {
-      field: 'prevBstpNo',
+      field: "prevBstpNo",
       enableRowGroup: true,
     },
     {
-      field: 'extRef',
+      field: "extRef",
       enableRowGroup: true,
     },
     {
-      field: 'prevExtRef',
+      field: "prevExtRef",
       enableRowGroup: true,
     },
     {
-      field: 'industryCode',
+      field: "industryCode",
       enableRowGroup: true,
     },
     {
-      field: 'userFieldValue1',
+      field: "userFieldValue1",
       enableRowGroup: true,
     },
     {
-      field: 'userFieldValue2',
+      field: "userFieldValue2",
       enableRowGroup: true,
     },
     {
-      field: 'copyRowAsNewYn',
+      field: "copyRowAsNewYn",
       enableRowGroup: true,
     },
     {
-      field: 'spoolNo',
+      field: "spoolNo",
       enableRowGroup: true,
     },
     {
-      field: 'segmId',
+      field: "segmId",
       enableRowGroup: true,
     },
     {
-      field: 'segmDescription',
+      field: "segmDescription",
       enableRowGroup: true,
     },
     {
-      field: 'budgetPerc',
+      field: "budgetPerc",
       enableRowGroup: true,
     },
     {
-      field: 'imprTotTgtAds',
+      field: "imprTotTgtAds",
       enableRowGroup: true,
     },
     {
-      field: 'imprTotActAds',
+      field: "imprTotActAds",
       enableRowGroup: true,
     },
     {
-      field: 'paybackNlin',
+      field: "paybackNlin",
       enableRowGroup: true,
     },
   ];
@@ -358,7 +470,7 @@ export class AppComponent implements OnInit {
     this.gridOptions = {
       suppressAggFuncInHeader: true,
       rowHeight: 34,
-      rowSelection: 'multiple',
+      rowSelection: "multiple",
       checkboxSelection: true,
 
       // columnDefs: this.colDefs,
@@ -381,9 +493,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.httpClient
-      .get<any>('assets/campaigns-725252-ad-server-adServerDetails.json')
+      .get<any>("assets/campaigns-725252-ad-server-adServerDetails.json")
       .subscribe((res) => {
-        console.log('data recieved', res);
+        console.log("data recieved", res);
 
         this.lmkData = res.data;
         console.log(Object.keys(this.lmkData[0]));
@@ -395,14 +507,14 @@ export class AppComponent implements OnInit {
           };
         });
 
-        var nameCol = colDefs.find((c) => (c.field = 'name'));
+        var nameCol = colDefs.find((c) => (c.field = "name"));
         //@
         nameCol.width = 600;
         this.colDefs = [nameCol, ...colDefs.filter((cd) => cd != nameCol)];
 
-        console.log('this.colDefs', this.colDefs);
-        console.log('this.lmkData', this.lmkData);
-        console.log('this.gridApi', this.gridApi);
+        console.log("this.colDefs", this.colDefs);
+        console.log("this.lmkData", this.lmkData);
+        console.log("this.gridApi", this.gridApi);
 
         // this.gridApi.setColDefs(this.colDefs);
         this.gridApi.setRowData(this.lmkData);
@@ -410,9 +522,9 @@ export class AppComponent implements OnInit {
   }
 
   onGridReady(params) {
-    console.log('grid ready');
-    this.gridApi = params.api;
-    this.gridColumnApi = params.columnApi;
-    this.gridOptionsApi = this.gridOptions.api;
+    // console.log("grid ready");
+    // this.gridApi = params.api;
+    // this.gridColumnApi = params.columnApi;
+    // this.gridOptionsApi = this.gridOptions.api;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 // ag-grid
 import { AgGridModule } from 'ag-grid-angular';
@@ -15,6 +16,7 @@ import { CheckboxRenderer } from './checkbox-renderer.component';
     FormsModule,
     HttpClientModule,
     AgGridModule.withComponents([CheckboxRenderer]),
+    MatCheckboxModule,
   ],
   declarations: [AppComponent, CheckboxRenderer],
   bootstrap: [AppComponent],
