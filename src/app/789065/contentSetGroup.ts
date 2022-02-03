103b69e41a24bb505ffc97bd88b8bc414ef84ac3
timothybroome <timothy@broome.me>
2022-02-03 14:05:38 +0000

    1 file added

---
+++ src/app/789065/contentSetGroup.ts
@@ -0,0 +1,66 @@
+const contentSet = {
+  "data": [
+      {
+          "cmodNo": 1,
+          "contentSetNo": 1,
+          "contentGroupNo": 17942,
+          "contentGroupDescription": "Sponsorship Group",
+          "activeYn": true,
+          "adTargetableYn": true,
+          "origSetNo": 1
+      },
+      {
+          "cmodNo": 2,
+          "contentSetNo": 1,
+          "contentGroupNo": 17942,
+          "contentGroupDescription": "Sponsorship Group",
+          "activeYn": true,
+          "adTargetableYn": true,
+          "origSetNo": 1
+      },
+      {
+          "cmodNo": 3,
+          "contentSetNo": 1,
+          "contentGroupNo": 17942,
+          "contentGroupDescription": "Sponsorship Group",
+          "activeYn": true,
+          "adTargetableYn": true,
+          "origSetNo" : 1
+      },
+      {
+          "cmodNo": 4,
+          "contentSetNo": 1,
+          "contentGroupNo": 17942,
+          "contentGroupDescription": "Sponsorship Group",
+          "activeYn": true,
+          "adTargetableYn": true,
+          "origSetNo": 1
+      },
+      {
+          "cmodNo": 5,
+          "contentSetNo": 1,
+          "contentGroupNo": 17942,
+          "contentGroupDescription": "Sponsorship Group",
+          "activeYn": true,
+          "adTargetableYn": true,
+          "origSetNo": 1
+      },
+      {
+          "cmodNo": 6,
+          "contentSetNo": 1,
+          "contentGroupNo": 17942,
+          "contentGroupDescription": "Sponsorship Group",
+          "activeYn": true,
+          "adTargetableYn": true,
+          "origSetNo": 1
+      }
+  ],
+  "totalCount": 6,
+  "filteredCount": 6,
+  "totalPages": 1,
+  "currentPage": 0,
+  "pageSize": 100,
+  "focusFound": false,
+  "focusPage": 0,
+  "warnings": []
+}
